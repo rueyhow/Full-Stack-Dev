b0ac9ce3c0eae716b69a5c972682d45f88088b25
h421qqq <haziqnadi@gmail.com>
2022-07-28 09:10:23 +0800

    a

--- node-main/HandyHandPhone/models/Product.js
+++ node-main/HandyHandPhone/models/Product.js
@@ -1,18 +1,19 @@
 const Sequelize = require('sequelize');
 const db = require('../config/DBConfig');
 
-class Product extends Sequelize.Model {}
+class Product extends Sequelize.Model { }
 
 Product.init(
     {
         name: { type: Sequelize.STRING },
         price: { type: Sequelize.DECIMAL },
         stock: { type: Sequelize.INTEGER },
-        description: { type: Sequelize.STRING }
+        description: { type: Sequelize.STRING },
+        productPic: { type: Sequelize.TEXT('medium')}
     },
     {
         sequelize: db,
         modelName: "product"
-    }); 
+    });
 
 module.exports = Product;
